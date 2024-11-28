fd54249360e15790cd1fe24972766253082b8098
water2027 <136900643+water2027@users.noreply.github.com>
2024-11-28 17:44:39 +0800

    异步加载页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
-import ProfileView from '../views/ProfileView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -35,7 +34,7 @@ const router = createRouter({
     {
       path: '/profile',
       name: 'profile',
-      component: ProfileView,
+      component: ()=>import('@/views/ProfileView.vue'),
     }
   ],
 });
