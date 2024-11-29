1432a314c1c4095e30f8aa061ae54eeb288ddc28
water2027 <136900643+water2027@users.noreply.github.com>
2024-11-29 16:37:46 +0800

    增加路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,26 +17,41 @@ const router = createRouter({
           path: 'save',
           name: 'save',
           component: () => import('@/views/SaveView.vue'),
+          meta:{
+            auth:true
+          }
         },
         {
           path: 'about',
           name: 'about',
           component: () => import('@/views/AboutView.vue'),
+          meta:{
+            auth:false
+          }
         },
         { 
           path: 'profile',
           name: 'profile',
           component: () => import('@/views/ProfileView.vue'),
+          meta:{
+            auth:true
+          }
         },
         {
           path: 'community',
           name: 'community',
           component: () => import('@/views/CommunityView.vue'),
+          meta:{
+            auth:false
+          }
         },
         {
           path: 'chat',
           name: 'chat',
           component: () => import('@/views/ChatView.vue'),
+          meta:{
+            auth:false
+          }
         }
         
       ],
@@ -50,20 +65,41 @@ const router = createRouter({
           path: 'login',
           name: 'login',
           component: () => import('@/views/LoginView.vue'),
+          meta:{
+            auth:false
+          }
         },
         {
           path: 'register',
           name: 'register',
           component: () => import('@/views/RegisterView.vue'),
+          meta:{
+            auth:false
+          }
         },
         {
           path: 'reset',
           name: 'reset',
           component: () => import('@/views/ResetView.vue'),
+          meta:{
+            auth:false
+          }
         },
       ],
     },
   ],
 });
+
+router.beforeEach((to,from,next)=>{
+  if(to.meta.auth){
+    if(localStorage.getItem('token')){
+      next();
+    }else{
+      next({name:'login'});
+    }
+  }else{
+    next();
+  }
+})
 
 export default router;
