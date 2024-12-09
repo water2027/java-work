746fabb4b1c85cbe4466ad5ab33956d158289f84
water2027 <136900643+water2027@users.noreply.github.com>
2024-12-09 20:38:38 +0800

    自动登录

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
+import { onMounted, watch } from 'vue';
+
+import { Login } from './api/UserApi/Login';
+onMounted(() => {
+  if (localStorage.getItem('rememberMe') === 'true') {
+    const email = localStorage.getItem('email') as string;
+    const password = localStorage.getItem('password') as string;
+    const { data, isLoading, err } = Login({
+      email: email,
+      password: password,
+    });
+    watch(isLoading,()=>{
+      if(err.value){
+        console.log(err);
+      }else{
+        if(!isLoading.value){
+          if(data.value?.token){
+            localStorage.setItem('token', data.value.token);
+          }
+        }
+      }
+    })
+  }
+});
 window.addEventListener('beforeunload', () => {
-  if(localStorage.getItem('rememberMe') === 'false'){
+  if (localStorage.getItem('rememberMe') === 'false') {
     localStorage.removeItem('token');
   }
 });
