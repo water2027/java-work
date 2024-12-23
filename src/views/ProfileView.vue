560a8bba65faf7dab25be0753c8e366176df1a19
water2027 <136900643+water2027@users.noreply.github.com>
2024-12-23 13:26:56 +0800

    修改头像

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -5,7 +5,7 @@ import { onMounted } from 'vue';
 const userStore = useUserStore(); // 获取 user store
 const { user, setAvatar } = userStore;
 
-const onProfileImageChange = (e: Event)=>{
+const onProfileImageChange = (e: Event) => {
   const file = (e.target as HTMLInputElement)?.files?.[0];
   // base64 编码
   const reader = new FileReader();
@@ -14,36 +14,41 @@ const onProfileImageChange = (e: Event)=>{
     setAvatar(base64);
     console.log(base64);
   };
-  file&&reader.readAsDataURL(file);
-}
+  file && reader.readAsDataURL(file);
+};
 
 const logoutHandler = () => {
   localStorage.removeItem('token');
-  localStorage.removeItem('email')
-  localStorage.removeItem('password')
-  localStorage.removeItem('rememberMe')
+  localStorage.removeItem('email');
+  localStorage.removeItem('password');
+  localStorage.removeItem('rememberMe');
   window.location.reload();
 };
 
-onMounted(()=>{
-  if(localStorage.getItem('avatar')){
-    setAvatar(localStorage.getItem('avatar') as string)
+onMounted(() => {
+  if (localStorage.getItem('avatar')) {
+    setAvatar(localStorage.getItem('avatar') as string);
   }
-})
-
+});
 </script>
 
 <template>
   <div class="p-5 max-w-[600px] mx-auto text-center">
     <div class="bg-white p-5 rounded-lg shadow text-center mb-5">
       <!-- 头像，默认占位图 -->
-      <img
-      :src="
-        user.profilePicture || 'https://via.placeholder.com/150'
-      "
-      alt="User Avatar"
-      class="w-[150px] h-[150px] rounded-full object-cover mb-5 mx-auto"
-      />
+      <label class="cursor-pointer inline-block">
+        <img
+          :src="user.profilePicture || 'https://via.placeholder.com/150'"
+          alt="User Avatar"
+          class="w-[150px] h-[150px] rounded-full object-cover mb-5"
+        />
+        <input
+          type="file"
+          class="hidden"
+          accept="image/*"
+          @change="onProfileImageChange"
+        />
+      </label>
       <!-- 显示用户名 -->
       <h2>{{ user.username || '未命名' }}</h2>
       <!-- 显示邮箱 -->
